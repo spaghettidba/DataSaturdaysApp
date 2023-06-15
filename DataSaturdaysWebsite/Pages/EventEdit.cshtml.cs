8f42a39fe1a1aaf6a7166178a360bc87ff22a87c
Frezza06 <135005698+Frezza06@users.noreply.github.com>
2023-06-15 17:39:01 +0200

    Event edit

---
+++ DataSaturdaysWebsite/Pages/EventEdit.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataSaturdaysWebsite.Pages
+{
+    public class EventEditModel : PageModel
+    {
+        private readonly ILogger<UserModel> _logger;
+
+        public EventEditModel(ILogger<UserModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
